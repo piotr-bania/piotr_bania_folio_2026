01c9d627fcbe45e16b49407a52eb70437aaa2453
Piotr Bania <83336214+piotr-bania@users.noreply.github.com>
2026-01-29 23:47:26 +0000

    feat: update Main_Model to use folio_map model; enhance Main_Scene with fog and new environment settings; add new HDR and GLB files

--- components/experience/Main_Model.js
+++ components/experience/Main_Model.js
@@ -3,7 +3,7 @@
 import { useGLTF } from "@react-three/drei"
 
 export default function Main_Model() {
-    const { scene } = useGLTF("/models/desk_scene.glb")
+    const { scene } = useGLTF("/models/folio_map.glb")
 
     return <primitive object={scene} />
 }
--- components/experience/Main_Scene.js
+++ components/experience/Main_Scene.js
@@ -9,9 +9,10 @@ import Camera_Controller from "@/components/experience/Camera_Controller"
 export default function Main_Scene() {
     return (
         <div className="fixed inset-0 z-0 pointers-events-none">
-            <Canvas dpr={[1, 1.5]} gl={{ antialias: true }}>
+            <Canvas dpr={[1, 1.5]}>
+                <fog attach="fog" args={["#f0eff1", 5, 150]} />
                 <Suspense fallback={null}>
-                    <Environment preset="studio" />
+                    <Environment files="/env/syferfontein_18d_clear_puresky_1k.hdr" />
                     <Camera_Controller />
                     <Main_Model />
                 </Suspense>
